180a89c7940e73fc4e5e8c7fb51e073348e0688f
kaibas01 <kaibas01@allegheny.edu>
2023-09-12 13:37:20 -0400

    basic form of the code

---
+++ Code/main.py
@@ -0,0 +1,28 @@
+
+def Main():
+    
+    print("Hi, this is a program that switches temperature between Fahrenheit and Celsius")
+
+    user_input = int(input("Enter the temperature in Fahrenheit (Only use numbers): "), end = "\n\n")
+
+    formula = 5/9 * (user_input - 32)
+
+    print(formula)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    Main()
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 A basic python code for calculating the temperature between Fahrenheit and Celsius
 
 ![This is the formula that I used in my code](image.png)
+
