03d8d82a702dd4df9f7ed59469e998c1e987294a
kaibas01 <kaibas01@allegheny.edu>
2023-09-12 15:46:05 -0400

    finishing the basic-code formula

--- Code/main.py
+++ Code/main.py
@@ -3,40 +3,24 @@ def Main():
     
     print("Hi, this is a program that switches temperature between Fahrenheit and Celsius", end = "\n\n")
 
-    user_input = int(input("Enter the temperature in Fahrenheit (Only use numbers): "))
+    user_input = str(input("What type of degree you want to switch?: " ))
+    degree_input = int(input(f"You selected {user_input}, now type the degree in digits: "))
 
-    #if user_input.format = "str"
+    #the two formulas for the calculation
+    formula_c = ((degree_input) - 32) * 5/9 
+    formula_f = (9/5 * (degree_input)) + 32
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    formula = (user_input - 32) * 5/9
-
-    print(f"{formula} Celsius degrees.")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    #the process of deciding the user input and doing the calculation
+    if user_input == "Fahrenheit":
+        print(formula_c)
+    elif user_input == "Celsius":
+        formula_f = (9/5 * (degree_input)) + 32
+        print(formula_f)
+    else:
+        print("You might misspelled the input!")
+    
+    
+   # print(f"{formula} Celsius degrees.")
 
 
 if __name__ == "__main__":
