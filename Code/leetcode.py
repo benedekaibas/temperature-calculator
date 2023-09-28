7defa2a8c2953b63f53f4e4aa5c8f6aa644baf85
kaibas01 <kaibas01@allegheny.edu>
2023-09-28 10:47:22 -0400

    first leetcode assignment

---
+++ Code/leetcode.py
@@ -0,0 +1,16 @@
+#COMMON PREFIX FINDER
+
+def main():
+    strs = ["cat", "dog", "door", "doorbell"]
+    first = strs[0]
+    last = len(strs) - 1
+    result = first, last
+    return result
+
+
+
+
+print(main())
+
+if __name__ == "__main__":
+    main()
