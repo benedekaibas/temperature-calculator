12047352933f98089a6b9ff95eab419c24dbc9ac
kaibas01 <kaibas01@allegheny.edu>
2023-10-02 22:10:15 -0400

    got in loop

---
+++ Code/guessgame.py
@@ -0,0 +1,32 @@
+import random 
+
+lower = 1
+upper = 2
+takeGuess = int(input("Take a guess: "))
+guess = random.randint(lower, upper)
+
+def lower():
+    return takeGuess < guess
+
+def upper():
+    return takeGuess > guess
+
+while True:
+    if (takeGuess != guess and lower() == True):
+        print("You did not find the number, plus you guessed number is under the secret number!")
+    elif (takeGuess != guess and upper() == True):
+        print("You did not find the number, plus your guessed number is above the secret number! ")
+    else:
+        print("You guessed the number!")
+        break
+
+
+
+
+
+
+
+
+
+
+
