fea377b054e355c3994ea593d35db62e676018ce
kaibas01 <kaibas01@allegheny.edu>
2023-10-13 09:10:32 -0400

    changes in code file

--- Code/code.py
+++ Code/code.py
@@ -8,12 +8,22 @@ while i >= start and i < 10:
     print(f"{i} : {i}")
 
 
+def range():
+    set = start, end
+    return set
 
+for i in range():
+    result = i - 1
+    print(result)
 
+a = 5 
+b = int(input("input: "))
 
-
-
-
+while a != b:
+    try: 
+        int(input("input again: "))
+    except ValueError:
+        print("The value you gave are not correct")
 
 
 
