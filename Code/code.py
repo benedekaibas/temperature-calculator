6dbfd3f010ce8c39c9d574cb4effa4e9a2cef6fd
kaibas01 <kaibas01@allegheny.edu>
2023-10-18 09:07:55 -0400

    class exercise

--- Code/code.py
+++ Code/code.py
@@ -1,33 +1,10 @@
-i = 0
-start = 0 
-end = 10
+#Write a program that returns data from a list based on the type of the data 
+#(Str, int, etc.) and stores them in different variables
 
+#I made this list for an example
+my_list = [1, 0.3, "abc"]
 
-while i >= start and i < 10:
-    i += 1
-    print(f"{i} : {i}")
-
-
-def range():
-    set = start, end
-    return set
-
-for i in range():
-    result = i - 1
-    print(result)
-
-a = 5 
-b = int(input("input: "))
-
-while a != b:
-    try: 
-        int(input("input again: "))
-    except ValueError:
-        print("The value you gave are not correct")
-
-
-
-
+print("dsa")
 
 
 
