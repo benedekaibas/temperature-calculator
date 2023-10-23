cd7e5f0862bd231914d64ea6de51bf94cb0e8d42
kaibas01 <kaibas01@allegheny.edu>
2023-10-23 09:05:09 -0400

    added a test method to the program

--- Code/leetcode.py
+++ Code/leetcode.py
@@ -5,8 +5,11 @@ numbers_y = [0,1,2,3,4,5,6,7,8,9,10]
 x = random.choice(numbers_x)
 y = random.choice(numbers_y)
 
+#this part checks if the first and second elements are actually correct
 random_coordinate = x, y
 print(random_coordinate)
+print(random_coordinate[0])
+print(random_coordinate[1])
 
 
 
