8df7557853a027d3cf013e9836148526f44756cf
kaibas01 <kaibas01@allegheny.edu>
2023-11-01 09:24:20 -0400

    changes in file

--- classwork.py
+++ classwork.py
@@ -16,16 +16,21 @@ print("number of elements in fruit: ", len(fruit))
 # get the number of times an item appear in the list
 print("count of apple: ", fruit.count("apple"))
 
+fruit_dict = {'apples': 4, 'bananas': 3, 'strawberries': 10, 'oranges': 2}
+print(fruit_dict)
 
+fruit_dict['pears'] = 6
+print(fruit_dict)
 
+fruit_dict['apples'] += 1
+print(fruit_dict)
 
+to_list = list(fruit_dict)
+print(to_list)
 
-
-
-
-
-
-
+#sorting the elements alphabetical order
+sorting = sorted(fruit_dict)
+print(sorting)
 
 
 
